7bcad00bf957d929b769b9ee70e3b79aa1affa5d
zgsgs <594zhaogaosheng@gmail.com>
2022-11-29 22:24:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Docker Deploy
+
+Docker 部署前后端项目
---
+++ web/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+async function getApiJson() {
+  const res = await fetch('/api/')
+  console.log(res);
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <div style="display: flex;gap: 8px;">
+      <button type="button" @click="count++">count is {{ count }}</button>
+      <button type="button" @click="getApiJson">Get Json</button>
+    </div>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
